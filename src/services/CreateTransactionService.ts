c2648cae36dfb6994ae85925ff6adc8039b73aa5
Fábio <fsmaiorano@gmail.com>
2020-04-21 19:19:13 +0100

    implement create category and transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,22 +1,26 @@
 import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
 import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateCategoryService from '../services/CreateCategoryService';
 import { getRepository, getCustomRepository } from 'typeorm';
+import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: string;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute({ title, value, type, category }: Transaction): Promise<Transaction> {
-    // var balance = this.transactionsRepository.getBalance();
-
-    // if (
-    //   transaction.type === 'outcome' &&
-    //   balance.income < balance.outcome + transaction.value
-    // ) {
-    //   throw Error('invalid income value');
-    // }
-
+  public async execute({ title, value, type, category }: Request): Promise<Transaction> {
     const transactionRepository = getRepository(Transaction);
 
-    const transaction = transactionRepository.create({ title, value, type, category });
+    const createCategoryService = new CreateCategoryService();
+
+    var selectedCategory = await createCategoryService.execute({ title: category });
+
+    const transaction = transactionRepository.create({ title, value, type, category_id: selectedCategory.id });
 
     await transactionRepository.save(transaction);
 
